9fe17c451901b366964aa2a33841664b4c3a2222
Gerardo Melkart Noeller Álva <gerardo.noeller@sspc.gob.mx>
2026-03-31 13:11:03 -0600

    cambios martes

--- coments/models.py
+++ coments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from elements.models import Element
 
-# Create your models here.
+class Comment(models.Model):
+    text = models.TextField()
+    date_posted = models.DateField(auto_now_add=True)
+    element = models.ForeignKey(Element, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Comment for {self.element.title if self.element else 'N/A'} - {self.date_posted.strftime('%Y-%m-%d')}"
