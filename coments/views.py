435431c4bc196a9bb23cfa6dd426af5bc644e919
Gerardo Melkart Noeller Álva <gerardo.noeller@sspc.gob.mx>
2026-03-31 19:01:53 -0600

    cambios martes noche

--- coments/views.py
+++ coments/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Comment
 
 # Create your views here.
+def add(request):
+    # return HttpResponse("Hello, World!")
+    if request.method == 'GET':
+        return render(request, 'coments/add.html') 
+    else:
+        coment = Comment()
+        coment.text = request.POST['text']
+        coment.save()
+        return HttpResponse(request.POST.get('text'))
