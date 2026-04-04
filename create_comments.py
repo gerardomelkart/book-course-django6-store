9b42b71d1c9b3b8141263430e8efd1135fdcf439
gerardomelkart <gerardomelkart@gmail.com>
2026-04-03 19:32:57 -0600

    viernes2

---
+++ create_comments.py
@@ -0,0 +1,20 @@
+from faker import Faker
+
+def main():
+    fake = Faker()
+    for _ in range(5):
+        comment = Comment.objects.create(
+            text=fake.text()
+        )
+        comments = Comment.objects.all()
+        print(f"Comments in DB: {comments.count()}")
+
+
+if __name__ == '__main__':
+    import os
+    from django.core.wsgi import get_wsgi_application
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mystore.settings')
+    application = get_wsgi_application()
+
+    from coments.models import Comment
+    main()
